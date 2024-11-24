c92702dd0c0c0bd4b917c55ea3f8c549a73293b7
Zach Javeri <zach.javeri@gmail.com>
2024-11-23 20:45:40 -0500

    Changed the navbar to remove options that were not used, added a package for making api calls to the server

--- client/src/components/navbar/Navbar.tsx
+++ client/src/components/navbar/Navbar.tsx
@@ -16,102 +16,23 @@ const Navbar = () => {
 	return (
 		<nav className='navbar navbar-expand-lg navbar-light bg-light'>
 			<a className='navbar-brand' href='/'>
-				Ecommerce Store
+				ShareSpace
 			</a>
-			<button
-				className='navbar-toggler'
-				type='button'
-				data-toggle='collapse'
-				data-target='#navbarSupportedContent'
-				aria-controls='navbarSupportedContent'
-				aria-expanded='false'
-				aria-label='Toggle navigation'
-			>
-				<span className='navbar-toggler-icon'></span>
-			</button>
-
-			<div className='collapse navbar-collapse' id='navbarSupportedContent'>
-				<ul className='navbar-nav mr-auto'>
-					<li className='nav-item active'>
-						<a className='nav-link' href='/'>
-							Home
+			<div className='ml-auto'>
+				<ul className='navbar-nav ml-auto'>
+					<li className='nav-item'>
+						<a className='nav-link' href='/signup'>
+							Sign up
 						</a>
 					</li>
-					{!cookies.user_name ? (
-						<React.Fragment>
-							<li className='nav-item'>
-								<a className='nav-link' href='/login'>
-									Login
-								</a>
-							</li>
-							<li className='nav-item'>
-								<a className='nav-link' href='/signup'>
-									Signup
-								</a>
-							</li>
-						</React.Fragment>
-					) : (
-						<React.Fragment>
-							<li className='nav-item active'>
-								<a className='nav-link' href='/order-history'>
-									Order History
-								</a>
-							</li>
-							<li className='nav-item'>
-								<a className='nav-link' href='/cart'>
-									Cart
-								</a>
-							</li>
-						</React.Fragment>
-					)}
-
-					{cookies.is_admin && cookies.is_admin === 'true' ? (
-						<li className='nav-item'>
-							<a className='nav-link' href='/create-product'>
-								Add Product
-							</a>
-						</li>
-					) : null}
+					<li className='nav-item'>
+						<a className='nav-link' href='/login'>
+							Login
+						</a>
+					</li>
 				</ul>
 			</div>
-
-			<form className='form-inline'>
-				<div className='row'>
-					<div className='col'>
-						<input
-							className='form-control mr-sm-2 search'
-							type='search'
-							placeholder='Search'
-							aria-label='Search'
-							value={searchString}
-							onChange={(e) => setSearchString(e.target.value)}
-						/>
-					</div>
-					<div className='col'>
-						<button
-							className='btn btn-outline-success my-2 my-sm-0'
-							type='button'
-						>
-							<a className='nav-link' href={`/search-results/${searchString}`}>
-								Search
-							</a>
-						</button>
-					</div>
-					{cookies.user_name ? (
-						<React.Fragment>
-							<div className='col'>
-								<p>Logged in as: {cookies.user_name}</p>
-							</div>
-
-							<div className='col'>
-								<button className='btn' type='button' onClick={logOut}>
-									Logout
-								</button>
-							</div>
-						</React.Fragment>
-					) : null}
-				</div>
-			</form>
+			
 		</nav>
 	);
 };
